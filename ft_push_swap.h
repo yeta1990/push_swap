4530ba25ecdaf5dd5f82faab319d732327641868
albgarci <albgarci@student.42madrid.com>
2021-10-08 20:37:04 +0200

    header

---
+++ ft_push_swap.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_push_swap.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albgarci <albgarci@student.42madrid.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/08 13:29:59 by albgarci          #+#    #+#             */
+/*   Updated: 2021/10/08 13:38:40 by albgarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PUSH_SWAP_H
+# define FT_PUSH_SWAP_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+int	ft_atoi(const char *str);
+
+#endif
